fd99b9dd67a36aff38ee9be72bcbb2505c4efa7f
KigoJomo <kigojomo@gmail.com>
2024-10-22 11:25:08 +0300

    Products Page Layout Update

--- app/components/Products/ProductListing.js
+++ app/components/Products/ProductListing.js
@@ -12,7 +12,7 @@ export default async function ProductListing() {
   const data = JSON.parse(file)
 
   return (
-    <section className="py-0 px-0 flex flex-col gap-0">
+    <section className="py-0 flex flex-col gap-0 bg-background">
       {data.map((category, index) => (
         <CategoryItem
           key={index}
@@ -24,42 +24,42 @@ export default async function ProductListing() {
   )
 }
 
-const Category = ({ title, services }) => {
-  return (
-    <div className="w-full px-4 md:px-32 py-12 flex flex-col gap-6 bg-white bg-opacity-75">
-      <h2 className="">{title}</h2>
+// const Category = ({ title, services }) => {
+//   return (
+//     <div className="w-full px-4 md:px-32 py-12 flex flex-col gap-6 bg-white bg-opacity-75">
+//       <h2 className="">{title}</h2>
 
-      <div className="w-full overflow-x-scroll scrollbar-hidden flex gap-8">
-        {services.map((service, index) => (
-          <ServiceItem key={index} {...service} />
-        ))}
-      </div>
-    </div>
-  )
-}
+//       <div className="w-full overflow-x-scroll scrollbar-hidden flex gap-8">
+//         {services.map((service, index) => (
+//           <ServiceItem key={index} {...service} />
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
 
-const ServiceItem = ({ title, slug, description, image }) => {
-  return (
-    <Link
-      href={`/products/${slug}`}
-      className="w-[60vw] md:w-[25vw] aspect-[9/16] md:aspect-[3/4] border border-background flex-shrink-0 flex flex-col gap-4  bg-background relative before:absolute before:z-[1] before:top-0 before:left-0 before:w-full before:h-full before:bg-cyan before:opacity-20 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300"
-    >
-      <div className="w-full aspect-square bg-gradient-to-tl from-indigo via-purple to-blue z-[5]"></div>
+// const ServiceItem = ({ title, slug, description, image }) => {
+//   return (
+//     <Link
+//       href={`/products/${slug}`}
+//       className="w-[60vw] md:w-[25vw] aspect-[9/16] md:aspect-[3/4] border border-background flex-shrink-0 flex flex-col gap-4  bg-background relative before:absolute before:z-[1] before:top-0 before:left-0 before:w-full before:h-full before:bg-cyan before:opacity-20 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300"
+//     >
+//       <div className="w-full aspect-square bg-gradient-to-tl from-indigo via-purple to-blue z-[5]"></div>
 
-      <div className="w-full h-[43.75%] px-4 pb-4 flex flex-col justify-end gap-4">
-        <h4 className="text-foreground font-bold z-[5]">{title}</h4>
+//       <div className="w-full h-[43.75%] px-4 pb-4 flex flex-col justify-end gap-4">
+//         <h4 className="text-foreground font-bold z-[5]">{title}</h4>
 
-        <p className="text-xs text-ellipsis whitespace-nowrap text-nowrap overflow-hidden opacity-50 z-[5]">
-          {description}
-        </p>
+//         <p className="text-xs text-ellipsis whitespace-nowrap text-nowrap overflow-hidden opacity-50 z-[5]">
+//           {description}
+//         </p>
 
-        <CustomLink
-          href={`/products/${slug}`}
-          text={'discover'}
-          arrow={false}
-          className={''}
-        />
-      </div>
-    </Link>
-  )
-}
+//         <CustomLink
+//           href={`/products/${slug}`}
+//           text={'discover'}
+//           arrow={false}
+//           className={''}
+//         />
+//       </div>
+//     </Link>
+//   )
+// }
--- app/components/Products/ServiceItem.js
+++ app/components/Products/ServiceItem.js
@@ -1,23 +1,37 @@
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
 const ServiceItem = ({ title, slug, description, image }) => {
   return (
     <Link
       href={`/products/${slug}`}
-      className="w- md:w-[25vw] aspect-[3/4] md:aspect-square border border-background flex-shrink-0 flex flex-col gap-4  bg-background relative before:absolute before:z-[1] before:top-0 before:left-0 before:w-full before:h-full before:bg-background before:opacity-20 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300"
+      className="w-full aspect-[3/4] md:aspect-square flex-shrink-0 flex flex-col gap-4   relative before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-gradient-to-tl from-purple to-indigo before:opacity-25 before:-z-0 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300"
     >
-      <div className="w-full aspect-square bg-gradient-to-tl from-indigo via-purple to-blue z-[5]"></div>
+      <div className="w-full h-full md:h-auto md:aspect-[4/3] md:p-2 md:z-[5] absolute md:static">
+        <Image
+          src={image}
+          alt={`Broadcom ${title}`}
+          width={500}
+          height={500}
+          className="w-full h-full object-cover border border-background"
+        />
+      </div>
 
-      <div className="w-full md:h-[43.75%] px-4 pb-4 flex flex-col justify-between md:justify-end gap-2 md:gap-4">
-        <p className="text-foreground text-lg font-bold opacity-100 z-[5]">{title}</p>
+      <div className="w-full h-full md:h-[43.75%] p-2 md:p-4 flex flex-col justify-end gap-2 md:gap-4 relative md:static before:absolute md:before:hidden before:h-full before:w-full before:top-0 before:left-0 before:bg-gradient-to-t from-[#000000ce] to-[#00000000]">
 
-        <p className="text-xs text-ellipsis whitespace-nowrap text-nowrap overflow-hidden flex-shrink-0 opacity-50 z-[5]">
+        <p className="text-background md:text-foreground text-lg font-bold opacity-100 z-[5]">
+          {title}
+        </p>
+
+        <p className="text-background md:text-foreground text-xs text-ellipsis whitespace-nowrap text-nowrap overflow-hidden flex-shrink-0 md:opacity-50 z-[5]">
           {description}
         </p>
 
-        <button className="capitalize bg-purple px-4 py-2 w-fit text-lg hidden md:flex items-center gap-8 hover:shadow-2xl transition-all duration-300">discover</button>
+        <button className="capitalize bg-purple px-4 py-2 w-fit text-lg hidden md:flex items-center gap-8 hover:shadow-2xl transition-all duration-300">
+          discover
+        </button>
 
-        <p className="flex md:hidden text-indigo ml-auto underline">View</p>
+        <p className="flex md:hidden text-cyan ml-auto underline">View</p>
       </div>
     </Link>
   )
