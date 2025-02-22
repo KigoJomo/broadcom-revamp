6ded638585bccf3962f70c987d86ddc65fad4122
AlvisNjenga <alvisnjenga59@gmail.com>
2025-02-22 03:34:45 +0300

    fixing the local repo

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,6 +3,7 @@ import Landing from '../components/Landing'
 import Hero from '../components/Contact/Hero'
 import ContactMethods from '../components/Contact/ContactMethods'
 
+
 export default function Contact() {
   return (
     <>
